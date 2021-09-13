acf604ddafef38d5c29a420ef82492577aff71cf
welt-verloren <75497787+welt-verloren@users.noreply.github.com>
2021-09-13 18:18:33 +0200

    Bemmerl

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,8 +1,8 @@
-<footer id="footer" class="smallPadding white smallBottomMargin width100 inlineBlock">
+<footer id="footer" class="width100 smallPadding black centeredText">
 	<?php if ($site->termsConditionsFile()->isNotempty()) { ?>
 		<span class="noUnderline floatLeft mediumRightMargin"><a href="<?= $site->termsConditionsFile()->toFile()->url() ?>">AGBs</a></span>
 	<?php } else if ($site->termsConditionsPage()->isNotEmpty()) { ?>
 		<span class="noUnderline floatLeft mediumRightMargin"><a href="<?= $site->termsConditionsPage()->toPage()->url() ?>">AGBs</a></span>
 	<?php } ?>
-	<span class="floatRight">&copy <?= $site->name()->html() ?></span>
+	<span>&copy <?= $site->name()->html() ?></span>
 </footer>
