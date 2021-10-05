9b4009a6e581c4573bd000d0c3bdcd86dc513f2e
welt-verloren <75497787+welt-verloren@users.noreply.github.com>
2021-10-05 16:30:41 +0200

    Zipf

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,7 @@
 <?php snippet("head"); ?>
 <body class="home">
 	<?php snippet("header"); ?>
-	<?php snippet("products"); ?>
+	<?php snippet("products", ['filter' => 'off']); ?>
 	<?php snippet("footer"); ?>
 	<?php snippet("overlayaccount"); ?>
 	<?php snippet("overlaycart"); ?>
