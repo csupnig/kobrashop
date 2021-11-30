c3fcb0cd81eb59b36afbbc519b8e120308f6b23c
welt-verloren <75497787+welt-verloren@users.noreply.github.com>
2021-11-30 19:04:25 +0100

    Gitti

--- site/templates/product.php
+++ site/templates/product.php
@@ -1,36 +1,4 @@
-<?php snippet("head");
-
-//Fetch available product variants
-$productVariants = $page->children()->filterBy("intendedTemplate", "productvariant")->listed();
-
-//Remove multiple entries for identical colors
-$productColors = [];
-$variantMap = [];
-foreach ($productVariants as $productVariant) {
-  $colorNumber = $productVariant->title()->html();
-  $variantMap["color".$colorNumber] = $productVariant->id();
-  if (!in_array($colorNumber, $productColors)) $productColors [] = $colorNumber;
-}
-
-//Determine product color
-if (isset($_GET["productColor"])) $productColor = $_GET["productColor"];
-else $productColor = $productColors[array_rand($productColors, 1)];
-
-//Determine product background
-$productBackground = "";
-$sweepingUsages = [];
-foreach ($page->sweeping()->split() as $sweepingUsage) {
-  $sweepingUsages [] = $sweepingUsage;
-}
-$brushingUsages = [];
-foreach ($page->brushing()->split() as $brushingUsage) {
-  $brushingUsages [] = $brushingUsage;
-}
-if ($page->usage() == "special") $productBackground = "special";
-else if ($page->usage() == "liquids") $productbackground = "liquids";
-else if ($page->usage() == "sweeping") $productBackground = $sweepingUsages[0];
-else if ($page->usage() == "brushing") $productBackground = $brushingUsages[0]; ?>
-
+<?php snippet("head"); ?>
 <body class="product backgroundColor<?= $productColor ?>" data-color="<?= $productColor ?>">
 <script>
   var productVariants = '<?=json_encode($variantMap) ?>';
